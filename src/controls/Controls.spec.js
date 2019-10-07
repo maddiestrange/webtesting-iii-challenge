cba1056e4068fc31fc4849c69954cf74a1db7125
maddiestrange <maddie.strange@gmail.com>
2019-10-06 19:00:09 -0500

    controls testing

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,7 +1,7 @@
 // Test away!
 import React from "react";
 import renderer from "react-test-renderer"; 
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 
 import Controls from './Controls'
 
@@ -9,4 +9,53 @@ describe("<Controls />", () => {
     it('renders at all', () => {
         render(<Controls />)
     });
+})
+
+describe("Control buttons", () => {
+    it('is open and unlocked', () => {
+        const closeSpy = jest.fn();
+        const { queryByText } = render(<Controls locked={false} closed={false} toggleClosed={closeSpy}/>);
+        const lockButton = queryByText('Lock Gate');
+        expect(lockButton);
+        expect(lockButton.disabled).toBe(true);
+
+        const closeButton = queryByText('Close Gate');
+        expect(closeButton);
+        expect(closeButton.disabled).toBe(false);
+        fireEvent.click(closeButton);
+        expect(closeSpy).toBeCalled();
+      });
+
+      it('is closed and unlocked', () => {
+        const openSpy = jest.fn(), lockSpy = jest.fn();
+        const { queryByText } = render(<Controls locked={false}
+                                                 closed={true}
+                                                 toggleClosed={openSpy}
+                                                 toggleLocked={lockSpy}/>);
+        const lockButton = queryByText('Lock Gate');
+        expect(lockButton);
+        expect(lockButton.disabled).toBe(false);
+        fireEvent.click(lockButton);
+        expect(lockSpy).toBeCalled();
+        
+        const openButton = queryByText('Open Gate');
+        expect(openButton);
+        expect(openButton.disabled).toBe(false);
+        fireEvent.click(openButton);
+        expect(openSpy).toBeCalled();
+        
+      });
+      it('is closed and locked', () => {
+        const unlockSpy = jest.fn();
+        const { queryByText } = render(<Controls locked={true} closed={true} toggleLocked={unlockSpy} />);
+        const unlockButton = queryByText('Unlock Gate');
+        expect(unlockButton);
+        expect(unlockButton.disabled).toBe(false);
+        
+        const openButton = queryByText('Open Gate');
+        expect(openButton);
+        expect(openButton.disabled).toBe(true);
+        fireEvent.click(unlockButton);
+        expect(unlockSpy).toBeCalled();
+      });
 })
